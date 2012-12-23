1add658a990564d2ae7951d21ed4aa91c9cba571
Charles Feduke <charles.feduke@gmail.com>
2012-12-23 13:55:25 -0500

    initial commit

---
+++ src/test/groovy/com/deploymentzone/textanalysis/ExampleTest.groovy
@@ -0,0 +1,18 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.deploymentzone.textanalysis
+
+import com.deploymentzone.textanalysis.Example
+
+/**
+ * Tests for the {@link Example} class.
+ */
+class ExampleTest
+    extends GroovyTestCase
+{
+    void testShow() {
+        new Example().show()
+    }
+}
---
+++ src/test/groovy/com/deploymentzone/textanalysis/HelperTest.groovy
@@ -0,0 +1,19 @@
+//
+// Generated from archetype; please customize.
+//
+
+package com.deploymentzone.textanalysis
+
+import com.deploymentzone.textanalysis.Helper
+import com.deploymentzone.textanalysis.Example
+
+/**
+ * Tests for the {@link Helper} class.
+ */
+class HelperTest
+    extends GroovyTestCase
+{
+    void testHelp() {
+        new Helper().help(new Example())
+    }
+}
